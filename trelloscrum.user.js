3bb7e78719e565404bf3857e9272f7912b2790d3
nyctef <nyctef@nyctef.com>
2013-01-09 21:56:32 +0000

    probably fixes the "appearing #numbers" bug
    the title of the cards contains a hidden span such as <span
    class="card-short-id hide">#6</span> which looks like trello's internal
    number for the card. In firefox, $title[0].text was including the text
    from this hidden element and overwriting it in the card title

--- trelloscrum.user.js
+++ trelloscrum.user.js
@@ -237,11 +237,12 @@ function ListCard(el, identifier) {
     this.refresh = function () {
         var $title = $card.find('a.list-card-title');
         if (!$title[0]) return;
-        var title = $title[0].text;
+        // $title[0].childNodes[0] is a hidden span that contains the internal number of the card
+        var title = $title[0].childNodes[1].textContent;
         parsed = title.match(regexp);
         points = parsed ? parsed[1] : -1;
         if ($card.parent()[0]) {
-            $title[0].textContent = title.replace(regexp, '');
+            $title[0].childNodes[1].textContent = title.replace(regexp, '');
             $badge.text(that.points);
             consumed ? $badge.addClass("consumed") : $badge.removeClass('consumed');
             $badge.attr({ title: 'This card has ' + that.points + (consumed ? ' consumed' : '') + ' storypoint' + (that.points == 1 ? '.' : 's.') })
